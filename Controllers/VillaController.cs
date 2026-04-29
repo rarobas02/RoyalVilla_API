a5342e140fde3ea70f3c68375e82bb8d7171ac66
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-04-29 22:11:03 +0800

    17. Route Name

--- Controllers/VillaController.cs
+++ Controllers/VillaController.cs
@@ -3,11 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace RoyalVilla_API.Controllers
 {
+    [Route("api/villa")]
     [ApiController]
     public class VillaController : ControllerBase
     {
         [HttpGet]
-        [Route("/villas")]
+        [Route("/getvillas")]
         public string GetVillas()
         {
             return "Get all villas";
