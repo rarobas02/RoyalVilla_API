fee120f8423eee8792455f36caa8fb8ea5ecd1a6
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-04-29 21:44:17 +0800

    15. Create First API Controller

---
+++ Controllers/VillaController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoyalVilla_API.Controllers
+{
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+    }
+}
