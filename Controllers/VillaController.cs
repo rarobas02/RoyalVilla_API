ca86b1167476090a239f2299b2c8a1a7c632a5bf
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-04-29 21:58:58 +0800

    16. First Endpoint in Action

--- Controllers/VillaController.cs
+++ Controllers/VillaController.cs
@@ -6,5 +6,11 @@ namespace RoyalVilla_API.Controllers
     [ApiController]
     public class VillaController : ControllerBase
     {
+        [HttpGet]
+        [Route("/villas")]
+        public string GetVillas()
+        {
+            return "Get all villas";
+        }
     }
 }
