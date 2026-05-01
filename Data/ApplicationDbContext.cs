6bcc8dde26d85ae5e7d2b04e027eedd022094fca
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-05-01 14:03:19 +0800

    22. Create Table

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RoyalVilla_API.Models;
 
 namespace RoyalVilla_API.Data
 {
     public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
     {
-
+        public DbSet<Villa> Villas { get; set; }
     }
 }
