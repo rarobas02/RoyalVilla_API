424c859a4945d7952f859087627e4661537c1b1b
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-05-01 21:43:48 +0800

    26. Optimize Get Villa by Id

--- Controllers/VillaController.cs
+++ Controllers/VillaController.cs
@@ -22,9 +22,25 @@ namespace RoyalVilla_API.Controllers
             return Ok(await _db.Villas.ToListAsync()); //return status if everything was ok -> this way we no longer blocking the thread
         }
         [HttpGet("{id:int}")]
-        public string GetVillasById([FromRoute] int id) // data come from url itself
+        public async Task<ActionResult<Villa>> GetVillasById(int id)
         {
-            return $"Get villa {id}";
+            try
+            {
+                if(id<=0)
+                {
+                    return BadRequest("Villa Id must be greater than 0");
+                }
+                var villa = await _db.Villas.FirstOrDefaultAsync(u => u.Id == id);
+                if(villa == null)
+                {
+                    return NotFound($"Villa with ID {id} was not found");
+                }
+                return Ok(villa);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"An Error occurred while retrieving villa with Id{id}: {ex.Message}");
+            }
         }
         [HttpGet("{id:int}/{name}")] //string is the default type so name is empty
         public string GetVillasByIdAndName([FromRoute] int id,[FromRoute] string name) 
