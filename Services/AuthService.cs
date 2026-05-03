679567c9a9d1d4021730a741f12c432995d97a72
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-05-03 14:18:14 +0800

    47. Auth Service Interface

---
+++ Services/AuthService.cs
@@ -0,0 +1,22 @@
+﻿using RoyalVilla_API.Models.DTO;
+
+namespace RoyalVilla_API.Services
+{
+    public class AuthService : IAuthService
+    {
+        public Task<bool?> IsEmailExistAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO?> LoginAsync(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDTO?> RegisterAsync(RegistrationRequestDTO registrationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using RoyalVilla_API.Models.DTO;
+
+namespace RoyalVilla_API.Services
+{
+    public interface IAuthService
+    {
+        Task<UserDTO?>RegisterAsync(RegistrationRequestDTO registrationRequestDTO);
+        Task<LoginResponseDTO?> LoginAsync(LoginRequestDTO loginRequestDTO);
+        Task<bool?> IsEmailExistAsync(string email);
+
+    }
+}
