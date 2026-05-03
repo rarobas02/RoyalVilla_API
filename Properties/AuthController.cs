3995b29466566dc34903af8a29afda84c817b738
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-05-03 14:11:54 +0800

    46. Auth Controller

---
+++ Properties/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RoyalVilla_API.Data;
+using RoyalVilla_API.Models;
+using RoyalVilla_API.Models.DTO;
+
+namespace RoyalVilla_API.Properties
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public AuthController()
+        {
+
+        }
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(typeof(ApiResponse<UserDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ApiResponse<UserDTO>>> Register(RegistrationRequestDTO registrationRequestDTO)
+        {
+            //auth service
+            var response = ApiResponse<UserDTO>.Ok(null, "Users created successfully");
+            return Ok(response);
+        }
+    }
+}
